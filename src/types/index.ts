8a691452543954cf4448279b6474074538879e57
Binkeurss <nvbinh271104@gmail.com>
2024-12-02 03:35:43 +0700

    Build(comp): build Pagination comp

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
+import { PaginationProps } from "./Pagination.type";
 import { InputProps } from "@/types/Input.type";
 import { ButtonProps } from "@/types/Button.type";
 
 export type { InputProps };
 export type { ButtonProps };
+export type { PaginationProps };
