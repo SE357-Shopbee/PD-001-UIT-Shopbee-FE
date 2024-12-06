7579c273a0f2904fd0a67dce1ca5a4f0cc52e6ed
Binkeurss <nvbinh271104@gmail.com>
2024-12-06 18:00:21 +0700

    Build(comp): Build Breadcrumb component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import { ButtonBase, ButtonOutline, ButtonSolid } from "@/components/Button";
 import { Pagination } from "@/components/Pagination";
 import { ProductCard } from "@/components/ProductCard";
 import { CategoryCard } from "@/components/CategoryCard";
+import { Breadcrumb } from "@/components/Breadcrumb";
 
 export { Input, InputRadio };
 export { ButtonBase, ButtonOutline, ButtonSolid };
@@ -11,3 +12,4 @@ export { Pagination };
 export { ProductCard };
 export { CategoryCard };
 export { CategoryModal };
+export { Breadcrumb };
